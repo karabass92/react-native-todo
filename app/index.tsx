6813a9e04b7707cdd6815e3a4d8c4855037aff7e
karabass92 <shatveryank@gmail.com>
2025-05-11 16:50:30 +0500

    init

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+
+const MainScreen = () => {
+  return (
+    <View>
+      <Text>
+        Main Screen
+      </Text>
+    </View>
+  );
+};
+
+export default MainScreen;
---
+++ constants/style.ts
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+
+export const style = StyleSheet.create({
+  layout: {
+    paddingBlockStart: 40,
+    paddingHorizontal: 12,
+  },
+});
